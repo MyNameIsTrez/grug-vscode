6c794a8ad3dba5fdaa76860d531555a5b1dad910
xijniN <xijnin@gmail.com>
2025-09-17 12:45:48 -0500

    Added LSP support

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { LanguageClientOptions } from 'vscode-languageclient';
+import { LanguageClient, ServerOptions } from "vscode-languageclient/node"
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// TODO: Automatically install Grug-LS if it's not found in PATH
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Extension "grug" is now active!');
-
-	// console.log(parser);
-	// console.log(c);
-	// console.log(parser.setLanguage(c));
+	console.log('Extension "grug" is now active!')
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -22,6 +21,30 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from grug!');
 	});
+	
+	const serverOptions: ServerOptions = {
+      run: {
+        command: "grug-ls",
+      },
+      debug: {
+        command: "grug-ls",
+      },
+    };
+
+    const clientOptions: LanguageClientOptions = {
+      documentSelector: [{ scheme: "file", language: "grug" }],
+    };
+
+	const client: LanguageClient = new LanguageClient(
+      "grug-ls",
+      "Grug-LS",
+      serverOptions,
+      clientOptions
+    );
+
+	client.start().catch((err) => `grug-ls start error: ${err}`);
+
+	context.subscriptions.push(client);
 
 	context.subscriptions.push(disposable);
 }
