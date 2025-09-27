9e68c5d2bfc6ffb260c0592fa8abb5480f69d789
xijniN <xijnin@gmail.com>
2025-09-26 19:15:58 -0500

    Updated to grug-ls v0.1.3

--- setup.sh
+++ setup.sh
@@ -2,10 +2,8 @@
 
 # Intended for when deploying a new grug-ls version
 
-# https://github.com/xijnim/grug-ls/releases/download/v0.1.1/grug-ls-aarch64-apple-darwin.tar.xz
-
 set -e
-VERSION=$(echo "v0.1.2")
+VERSION=$(echo "v0.1.3")
 
 WINDOWS_TARGET=$(echo "x86_64-pc-windows-msvc")
 TARGETS=($WINDOWS_TARGET "aarch64-apple-darwin" "x86_64-apple-darwin" $"aarch64-unknown-linux-gnu" "x86_64-unknown-linux-gnu")
@@ -26,15 +24,12 @@ get_extension() {
 
 for target in "${TARGETS[@]}"
 do
-
     EXTENSION=$(get_extension $target)
     URL=$(echo "https://github.com/xijnim/grug-ls/releases/download/$VERSION/grug-ls-$target.$EXTENSION")
     wget $URL -O "bin/$target.$EXTENSION"
 
-
     if [ $EXTENSION = "tar.xz" ]; then
         tar xvf "bin/$target.$EXTENSION" -C "bin"
-        # mv "bin/grug-ls-$target/" "bin/$target"
         mv "bin/grug-ls-$target" "bin/$target"
     fi
     if [ $EXTENSION = "zip" ]; then
